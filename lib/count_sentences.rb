3a78fb636877c17c5324a3688699c8dcb3f3e975
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-01 04:24:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -25,7 +25,8 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+    self.split(/[.?!]+/).count
+#    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+#    return new_array.length
   end
 end
