3ccf2baec24bff6c0534721be1d151fb74ee823b
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-01 04:26:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,27 +1,15 @@
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    self.end_with?(".") ? true : false
   end
 
   def question?
     self.end_with?("?") ? true : false
-      return true
-    else
-      return false
-    end
   end
 
   def exclamation?
     self.end_with?("!") ? true : false
-#      return true
-#    else
-#      return false
-#    end
   end
 
   def count_sentences
